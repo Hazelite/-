86704e3135ba48a8b058779e324c7ed5ccae215e
olive <oliver.hazley@gmail.com>
2024-09-12 19:48:17 +0300

    new features, price page

--- script.js
+++ script.js
@@ -2,3 +2,52 @@ window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+// Mobile navigation toggle
+var CSbody = document.querySelector('body');
+const CSnavbarMenu = document.querySelector('#cs-navigation');
+const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
+
+CShamburgerMenu.addEventListener('click', function () {
+    CShamburgerMenu.classList.toggle('cs-active');
+    CSnavbarMenu.classList.toggle('cs-active');
+    CSbody.classList.toggle('cs-open');
+    ariaExpanded();
+});
+
+// Dark mode toggle
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const darkModePreference = window.matchMedia('(prefers-color-scheme: dark)');
+const body = document.body;
+
+function setDarkMode(isDarkMode) {
+    if (isDarkMode) {
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+    }
+}
+
+// Check for existing preference in localStorage
+const savedDarkMode = localStorage.getItem('darkMode');
+if (savedDarkMode) {
+    setDarkMode(savedDarkMode === 'true');
+} else {
+    setDarkMode(darkModePreference.matches);
+}
+
+darkModeToggle.addEventListener('click', () => {
+    const isDarkMode = body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
+});
+
+function ariaExpanded() {
+    const csUL = document.querySelector('#cs-expanded');
+    const csExpanded = csUL.getAttribute('aria-expanded');
+
+    if (csExpanded === 'false') {
+        csUL.setAttribute('aria-expanded', 'true');
+    } else {
+        csUL.setAttribute('aria-expanded', 'false');
+    }
+}
